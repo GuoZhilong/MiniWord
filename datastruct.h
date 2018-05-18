b41a63da07e7908f4ef3cee2b584a4b42b6c3c97
GuoZhilong <39120200+GuoZhilong@users.noreply.github.com>
2018-05-18 20:15:16 +0800

    接口改变及添加光标
    主数据结构中添加了光标变量
    class Cursor cursor
    并创建了新的插入和删除函数作为接口，默认在光标处做插入和删除操作，
    但还缺少对文字类型的控制，原先的函数移入private中。

--- datastruct.h
+++ datastruct.h
@@ -8,62 +8,62 @@
 #define state_saved 2
 class TextBlock {//一行的最小单位，长度为100的块
 private:
-	char* text;
-	TextBlock *next_block;
+    char* text;
+    TextBlock *next_block;
 public:
-	friend class TextNode;
+    friend class TextNode;
     friend class MainWindow;
-	TextBlock() {
-		text = new char[SIZE];
-		next_block = NULL;
-	}
-	~TextBlock() {
-		delete[] text;
-		next_block = NULL;
-	}
+    TextBlock() {
+        text = new char[SIZE];
+        next_block = NULL;
+    }
+    ~TextBlock() {
+        delete[] text;
+        next_block = NULL;
+    }
 };
 
 class TextNode{//指示一行的结点
 private:
-	TextNode * nextnode;//下一行对应结点
-	TextBlock * first;
-	TextBlock * end;
-	int size;//当前容量
-	int length;//当前长度
+    TextNode * nextnode;//下一行对应结点
+    TextBlock * first;
+    TextBlock * end;
+    int size;//当前容量
+    int length;//当前长度
 public:
     friend class Text;
     friend class MainWindow;
     TextNode():size(0),length(0){
         nextnode = NULL;
-		first = NULL;
-		end = NULL;
+        first = NULL;
+        end = NULL;
     }
     ~TextNode(){
-		TextBlock *cur, *next;
-		cur = first;
-		while (cur) {
-			next = cur->next_block;
-			delete cur;
-			cur = next;
-		}
+        TextBlock *cur, *next;
+        cur = first;
+        while (cur) {
+            next = cur->next_block;
+            delete cur;
+            cur = next;
+        }
         nextnode = NULL;
     }
     int Index(std::string string_aim, int position);
     void Input(std::string s);//用字符串s作为此行内容
     void Extend();//扩充容量
     void Renew();//重分配容量
-	//void Insert(std::string::const_iterator begin, std::string::const_iterator end, int position);
+    //void Insert(std::string::const_iterator begin, std::string::const_iterator end, int position);
     char& operator [](int i) {
         if (i < size) {
-			int j = 0;
-			TextBlock *cur = first;
-			while (j < i / SIZE) {
-				cur = cur->next_block;
-				++j;
-			}
+            int j = 0;
+            TextBlock *cur = first;
+            while (j < i / SIZE) {
+                cur = cur->next_block;
+                ++j;
+            }
             return cur->text[i - j *SIZE];
-		}
-	}
+        }
+    }
 };
 
 
@@ -78,20 +78,28 @@ private:
 };
 
 
-
+class Cursor{
+private:
+    int line;
+    int position;
+public:
+    friend class Text;
+    Cursor():line(1), position(1){}
+    Cursor(int l, int p): line(l), position(p){}
+};
 
 class Text{//代表文本的主数据结构
 public:
+    enum direction {left,right,up,down};
     friend class MainWindow;
-    Text():block_text(){
-
+    Text():block_text(), cursor(1,1){
        headnode = NULL;
        tailnode = NULL;
        lines = 0;
     }
-		Text(std::string Filename) {
-			this->Text_Set(Filename);
-		}
+    Text(std::string Filename) {
+       this->Text_Set(Filename);
+    }
     ~Text(){//析构函数
         TextNode *curnode = headnode;
         while(curnode){
@@ -106,10 +114,13 @@ public:
     void Save_File(bool Name_Choice);
     void Quit_File();
     bool Text_Set(std::string Filename);//根据提供文件名创建此类
-    bool Insert(int line, int position, std::string s);//向文本中某行某位置插入字符串
-    bool Delete(int line, int position);//删除某行某位置的一个字符
-    bool Index(std::string string_aim, int line, int position, int &aimline, int &aimposition);//从某行某位置后面开始匹配目标字符串，若成功则返回字串的头位置。
     bool Replace(std::string string_aim, std::string string_replace, int position, int line);//从某行某位置后面开始匹配目标字符串并把其用另一个串代替
+    bool Index(std::string string_aim, int line, int position, int &aimline, int &aimposition);//从某行某位置后面开始匹配目标字符串，若成功则返回字串的头位置。
+    void MoveCursor(direction dir);
+    void Insert_at_Cursor(std::string s){
+        Insert(cursor.line,cursor.position,s);
+    }
+    void Delete_at_Cursor(direction dir);
     bool Block_Set(int line_begin, int line_end, int position_begin, int positon_end);//设置块
     bool Block_Delete();//删除块
     bool Block_Copy();//复制块
@@ -127,15 +138,16 @@ public:
         headnode = new TextNode;
         tailnode = headnode;
     }
-		int Get_Max_Length(){
-				int max_length = 0;
-				TextNode *cur = headnode;
-				while(cur){
-					max_length = max_length > cur->length ? max_length : cur->length;
-					cur = cur->nextnode;
-				}
-				return max_length;
-		}
+    int Get_Max_Length(){
+        int max_length = 0;
+        TextNode *cur = headnode;
+        while(cur){
+            max_length = max_length > cur->length ? max_length : cur->length;
+            cur = cur->nextnode;
+        }
+        return max_length;
+    }
+
 private:
     std::fstream file;//文件
     std::string filename;//当前文件名
@@ -143,4 +155,7 @@ private:
     TextNode* tailnode;//尾结点
     int lines;//行数
     Block block_text;//块
+    Cursor cursor;
+    bool Insert(int line, int position, std::string s);//向文本中某行某位置插入字符串
+    bool Delete(int line, int position);//删除某行某位置的一个字符
 };
